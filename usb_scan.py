9fc34351c7a2e5bbe1e4aa1bee361806af4b3db3
ehsan.tafehi <ehsan.tafehi@gmail.com>
2021-06-07 17:51:58 +0200

    modify python code

--- usb_scan.py
+++ usb_scan.py
@@ -6,9 +6,16 @@ import usb.util
 
 # Add your idVendor and idProduct using lsusb in linux
 dev = usb.core.find(idVendor=0x0781, idProduct=0x5567)
-if not dev:
-    print("No device has been found :(")
-    exit(1)
-else:
 
-    print("Yeey. Find a device")
+
+def main():
+    if not dev:
+        print("No device has been found :_(")
+    else:
+
+        print("Yeey. Find a device")
+        time.sleep(5 * 60)
+
+
+if __name__ == "__main__":
+    main()
