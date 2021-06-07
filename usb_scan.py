b6c2a5f6f64ec4d0efb511a50005d505f5fb8a17
ehsan.tafehi <ehsan.tafehi@gmail.com>
2021-06-07 17:11:03 +0200

    modify idVendor and idProduct

--- usb_scan.py
+++ usb_scan.py
@@ -4,10 +4,11 @@ import usb.core
 import time
 import usb.util
 
-VID=0x0781, PID=0x5567 # Add your VID and PID using lsusb in linux
-dev = usb.core.find(idVendor=VID, idProduct=PID)
+# Add your idVendor and idProduct using lsusb in linux
+dev = usb.core.find(idVendor=0x0781, idProduct=0x5567)
 if not dev:
     print("No device has been found :(")
     exit(1)
 else:
+
     print("Yeey. Find a device")
