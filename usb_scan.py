a4d564f75c40dfb877159a4351c026340a87a992
ehsan.tafehi <ehsan.tafehi@gmail.com>
2021-06-07 21:10:09 +0200

    add device and config to python

--- usb_scan.py
+++ usb_scan.py
@@ -5,16 +5,17 @@ import time
 import usb.util
 
 # Add your idVendor and idProduct using lsusb in linux
-dev = usb.core.find(idVendor=0x0781, idProduct=0x5567)
-
+dev = usb.core.find(idVendor=0xffff, idProduct=0x5678)
+conf = dev.get_active_configuration()
 
 def main():
     if not dev:
         print("No device has been found :_(")
     else:
-
+        
         print("Yeepy. Usb device is found :)")
-        time.sleep(5)
+        print(f"The size of the Usb is {dev.get_active_configuration().wTotalLength}")
+        time.sleep(5) #TODO change 5 sec. to 5 min.
 
 
 if __name__ == "__main__":
