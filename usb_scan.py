317c5e31be4c4da80f113c7349cf3a9d108f5536
ehsan.tafehi <ehsan.tafehi@gmail.com>
2021-06-08 10:58:40 +0200

    add endpoint and interface

--- usb_scan.py
+++ usb_scan.py
@@ -1,22 +1,40 @@
 #!/usr/bin/python3
 
+
+
 import usb.core
 import time
 import usb.util
 
 # Add your idVendor and idProduct using lsusb in linux
 dev = usb.core.find(idVendor=0xffff, idProduct=0x5678)
-conf = dev.get_active_configuration()
 
+# find our device
 def main():
-    if not dev:
-        print("No device has been found :_(")
+    # was it found?
+    if dev is None:
+        raise ValueError('Device not found :(')
     else:
-        
-        print("Yeepy. Usb device is found :)")
-        print(f"The size of the Usb is {dev.get_active_configuration().wTotalLength}")
-        time.sleep(5) #TODO change 5 sec. to 5 min.
 
+    # set the active configuration. With no arguments, the first
+    # configuration will be the active one
+        print('Device is Found :)')
+        #get an endpoint
+        # get active configuration
+        cfg = dev.get_active_configuration()
+        #show the usb size
+        print(f"The usb size is {cfg.wTotalLength}")
+        #get interface description
+        intf = cfg[(0,0)]
+        #print(intf)
+        #print("=---------------------=====================-----------------")
+        # get endpoint 0x1
+        ep1 = intf[0]
+        #print(ep1)
+        # get endpoint 0x82
+        ep82 = intf[1]
+        time.sleep(5) #TODO change 5 sec. to 5 min.
+        #print(ep82)
 
 if __name__ == "__main__":
     main()
